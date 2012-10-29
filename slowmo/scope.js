cb8b155a0f0fbe57a97dfe05ef3867492f6a2f0f
Atul Varma <varmaa@gmail.com>
2012-10-29 13:03:46 -0400

    more logging in scope object

--- slowmo/scope.js
+++ slowmo/scope.js
@@ -15,22 +15,27 @@ define(function() {
     this.name = name;
     this.log = log || this.prev.log;
     this.vars = {};
+    this.range = range;
+    this.log("enter");
   }
 
   Scope.prototype = {
-    assign: function(name, operator, value) {
+    assign: function(name, operator, value, range) {
       if (name in this.vars) {
+        var oldValue = this.vars[name];
         switch (operator) {
           case "=":
-          return this.vars[name] = value;
+          this.vars[name] = value;
 
           default:
           throw new Error("unimplemented operator: " + operator);
         }
+        this.log("assign", name, operator, value, oldValue, range);
+        return this.vars[name];
       }
       return this.prev.assign(name, operator, value);
     },
-    update: function(name, operator, prefix) {
+    update: function(name, operator, prefix, range) {
       if (name in this.vars) {
         var oldValue = this.vars[name];
         switch (operator) {
@@ -40,17 +45,19 @@ define(function() {
           default:
           throw new Error("unimplemented operator: " + operator);
         }
+        this.log("update", name, operator, prefix, oldValue, range);
         return prefix ? this.vars[name] : oldValue;
       }
       return this.prev.update(name, operator, prefix);
     },
-    declare: function(name, value) {
+    declare: function(name, value, range) {
       this.vars[name] = value;
+      this.log("declare", name, value, range);
       return value;
     },
-    get: function(name) {
+    get: function(name, range) {
       if (name in this.vars) {
-        this.log("get", name, this.vars[name]);
+        this.log("get", name, this.vars[name], range);
         return this.vars[name];
       }
       return this.prev.get(name);
